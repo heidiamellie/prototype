e8e01fc9932bc3d2dfab47e11f66302f929f7189
Heidi Amellie Ayu Astria <heidiamellie@students.undip.ac.id>
2019-08-21 21:44:59 +0700

    bikin card baru sendiri

--- application/views/enam.php
+++ application/views/enam.php
@@ -1,26 +1,41 @@
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
-apa aja deh tulias apa aja yang panjang banget ajdhamdbxahgdknbdjhabcnabhcabsnfabsjhcbvdhabsdhag
+<div class="row">
+
+
+  <div class="card col-sm-7">
+    <div class="card-header">
+      CARD
+    </div>
+    <div class="card-body">
+      ASSALAMUALAIKUM
+    </div>
+    <div class="card-footer">
+      WASAALMULIAKUM
+    </div>
+  </div>
+
+
+
+  <div class="card col-sm-5">
+    <div class="card-header">
+      CARD
+    </div>
+    <div class="card-body">
+      ASSALAMUALAIKUM
+    </div>
+    <div class="card-footer">
+      WASAALMULIAKUM
+    </div>
+  </div>
+</div>
+
+<div class="card col-sm-12">
+  <div class="card-header">
+    CARD 2
+  </div>
+  <div class="card-body">
+    BISMILLAH
+  </div>
+  <div class="card-footer">
+    translate_uri_dashes
+  </div>
+</div>
